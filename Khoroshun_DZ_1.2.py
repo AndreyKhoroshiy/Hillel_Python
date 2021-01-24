1335d51324831777aa0c1a242ea019d236e18d4f
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-01-24 21:49:17 +0200

    Добавление assert-ов по второй задаче

--- Khoroshun_DZ_1.2.py
+++ Khoroshun_DZ_1.2.py
@@ -25,3 +25,9 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima ') == {'name': 'Dima'}
+    assert parse_cookie('name=Dima;age=28;sex=male') == {'name': 'Dima', 'age': '28', 'sex': 'male'}
+    assert parse_cookie('name') == {}
+    assert parse_cookie('name&Dima;age&28;') == {}
+    assert parse_cookie(' name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie(';name=Dima;') == {'name': 'Dima'}
